28792fccd0246341f2b227b4ac9444c0131b7cf8
Subhrajyoti Basu <44311167+subhrajyoti-basu@users.noreply.github.com>
2022-05-31 22:23:01 +0530

    Update arweave.js

--- src/utils/arweave.js
+++ src/utils/arweave.js
@@ -1,13 +1,13 @@
 import Arweave from 'arweave';
 
-export const arweave = Arweave.init({
-    host: 'arweave.net',
-    port: 443,
-    protocol: 'https'
-});
-
 // export const arweave = Arweave.init({
-//     host: 'localhost',
-//     port: 1984,
-//     protocol: 'http'
+//     host: 'arweave.net',
+//     port: 443,
+//     protocol: 'https'
 // });
+
+export const arweave = Arweave.init({
+    host: 'localhost',
+    port: 1984,
+    protocol: 'http'
+});
